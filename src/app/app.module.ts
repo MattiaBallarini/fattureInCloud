7a4ef34b7c1ffcbad9b3700325e1dd2d8d43a81d
MattiaBallarini <ballarini.mattia1990@gmail.com>
2022-01-10 18:51:41 +0100

    livello mese proporzionale e migliorie grafiche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AndamentoMensileComponent } from './components/andamento-mensile/andamento-mensile.component';
+
+
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it'
+registerLocaleData(localeIt);
 
 @NgModule({
   declarations: [
@@ -14,7 +19,9 @@ import { AndamentoMensileComponent } from './components/andamento-mensile/andame
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'it' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/andamento-mensile/andamento-mensile.component.ts
+++ src/app/components/andamento-mensile/andamento-mensile.component.ts
@@ -11,15 +11,22 @@ import { Anno, Mese } from './anno';
 export class AndamentoMensileComponent implements OnInit {
 
   anno?: Mese[];
-  premuto: boolean = false;
+  premuto: boolean = false; //variabile per tenere traccia dello stato del mouse
+
+  importi: number[] = []; //array che contiene tutti gli importi annuali (su cui verrà eseguito il metodo Math.max per ricavare l'importo maggiore)
+  importoMax: number = 0; //importo maggiore di tutto l'anno
 
   constructor( public andamentoService : AndamentoMensileService) { }
 
 
   ngOnInit(): void {
+    this.importi = [];
+
     this.andamentoService.getAnno().subscribe({
       next: risultato =>{
         this.anno = risultato.mesi;
+        this.anno.forEach((e) => {this.importi?.push(e.importo)}); //pusho gli importi mensili importi nell'array
+        this.importoMax = Math.max(...this.importi); //ricavo l'importo maggiore
       }
     })
   }
